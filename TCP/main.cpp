3a44cdc0626ac8c927a5681e40b754ee606f13c5
IOSA33 <sedinkin33@gmail.com>
2026-01-14 14:08:33 +0200

    template

---
+++ TCP/main.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include "src/Server.h"
+#include "src/Request/Request.h"
+#include "src/Response/Response.h"
+
+int main() {
+    Server server{ "0.0.0.0" , 6788 };
+
+    server.Get("/redirect", [](const auto& req, const auto& res) -> void {
+        // Example
+        // res.setStatus(404);
+        // res.setResponse("example response");
+    });
+
+    int err { server.run() };
+    if (err == 1) {
+        std::cout << "Error" << std::endl;
+    }
+}
---
+++ TCP/src/Server.h
@@ -0,0 +1,27 @@
+#pragma once
+
+#include <string>
+#include <functional>
+#include <unordered_map>
+
+class Request;
+class Response;
+
+class Server {
+private:
+    int m_port{};
+    std::string m_ip{};
+    
+    std::unordered_map<std::string, std::function<void(const Request&, const Response&)>> m_routes{};
+
+public:
+    Server(const std::string& ip, int port) 
+        : m_port(port), m_ip(ip) {}
+    int run();
+
+    // Requests
+    void Get(const std::string& path, const auto& lambda);
+    void Post(const std::string& path, const auto& lambda);
+    void Delete(const std::string& path, const auto& lambda);
+    void Updata(const std::string& path, const auto& lambda);
+};
